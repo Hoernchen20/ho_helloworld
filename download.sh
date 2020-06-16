5acd5a07e9fca14895fd727036aaccc7f7d06e0e
Hoernchen20 <felix@horn-witten.de>
2020-06-16 18:31:38 +0200

    add simple download script

---
+++ download.sh
@@ -0,0 +1,3 @@
+!/bin/bash
+arm-none-eabi-objcopy -O ihex bin/bluepill/ho_helloworld.elf bin/bluepill/ho_helloworld.hex
+~/Downloads/stm32flash/./stm32flash -w bin/bluepill/ho_helloworld.hex -v /dev/ttyUSB0 -b 576000
